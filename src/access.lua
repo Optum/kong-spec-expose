735671655e9e0e50f7be8e2d6bd064b968610a20
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2018-06-01 11:38:51 -0400

    Create access.lua

---
+++ src/access.lua
@@ -0,0 +1,27 @@
+local _M = {}
+local pl_stringx = require "pl.stringx"
+local http = require "resty.http"
+local responses = require "kong.tools.responses"
+
+function _M.run(conf)
+   if ("GET" == ngx.req.get_method() and pl_stringx.endswith(ngx.var.request_uri, "/specz")) then
+      local httpc = http:new()
+      local res, err = httpc:request_uri(conf.spec_url, {
+  	  method = "GET",
+	    ssl_verify = false,
+	    headers = {}
+	    })
+
+      if err then
+        return responses.send_HTTP_INTERNAL_SERVER_ERROR()
+      else
+        --Set a proper Content-Type header based on what backend returned
+	      ngx.header['Content-Type'] = res.headers['Content-Type']
+	      ngx.status = res.status
+	      ngx.print(res.body)
+	      ngx.exit(200)
+      end
+   end
+end
+
+return _M
