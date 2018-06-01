63826f65720ff8debd8dc1ccc2b2c3066a4c632f
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2018-06-01 11:43:28 -0400

    Update access.lua

--- src/access.lua
+++ src/access.lua
@@ -8,18 +8,18 @@ function _M.run(conf)
       local httpc = http:new()
       local res, err = httpc:request_uri(conf.spec_url, {
   	  method = "GET",
-	    ssl_verify = false,
-	    headers = {}
-	    })
+	  ssl_verify = false,
+	  headers = {}
+	  })
 
       if err then
         return responses.send_HTTP_INTERNAL_SERVER_ERROR()
       else
         --Set a proper Content-Type header based on what backend returned
-	      ngx.header['Content-Type'] = res.headers['Content-Type']
-	      ngx.status = res.status
-	      ngx.print(res.body)
-	      ngx.exit(200)
+	ngx.header['Content-Type'] = res.headers['Content-Type']
+	ngx.status = res.status
+	ngx.print(res.body)
+	ngx.exit(200)
       end
    end
 end
