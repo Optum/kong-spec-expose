6b1cdffe6350ea09988e997ef5d39ecfa3c6d346
jeremyjpj0916 <31913027+jeremyjpj0916@users.noreply.github.com>
2020-06-11 11:20:07 -0400

    Update handler.lua

--- src/handler.lua
+++ src/handler.lua
@@ -3,7 +3,7 @@ local access = require "kong.plugins.kong-spec-expose.access"
 local KongSpecExpose = {} 
 
 KongSpecExpose.PRIORITY = 2400
-KongSpecExpose.VERSION = 0.3
+KongSpecExpose.VERSION = "2.0.0"
 
 function KongSpecExpose:access(conf)
 	access.run(conf)
